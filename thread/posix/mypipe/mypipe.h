585085a43e25c8d68fe5fe341a44821da0e204ad
lihuiqin <lihuiqin@uplooking.com>
2018-02-05 11:19:37 +0800

    log__

--- thread/posix/mypipe/mypipe.h
+++ thread/posix/mypipe/mypipe.h
@@ -3,9 +3,16 @@
 
 #define PIPESIZE	1024
 
+#define PIPE_READ	0x00000001UL
+#define PIPE_WRITE	0x00000002UL
+
 typedef void mypipe_t;
 
 mypipe_t *mypipe_init(void);
+
+int mypipe_register(mypipe_t *,int opmap);
+
+int mypipe_unregister(mypipe_t *,int opmap);
 
 ssize_t mypipe_read(mypipe_t *, void *buf, size_t count);
 
