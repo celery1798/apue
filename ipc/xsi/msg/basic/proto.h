02dd700ef11f1628b1b21bb9b96e1590d8d5fb4b
lihuiqin <lihuiqin@uplooking.com>
2018-02-06 14:46:19 +0800

    log__

---
+++ ipc/xsi/msg/basic/proto.h
@@ -0,0 +1,20 @@
+#ifndef PROTO_H__
+#define PROTO_H__
+
+#define NAMESIZE	32
+
+#define KEYPATH		"/etc/services"
+#define KEYPROJ		'x'
+
+struct msg_st
+{
+	long mtype;
+	char name[NAMESIZE];
+	int math;
+	int chinese;
+};
+
+
+
+#endif
+
